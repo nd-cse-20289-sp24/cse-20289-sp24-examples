0888d97376890cbe70912db479b34f1ece4e0ce7
Peter Bui <pbui@nd.edu>
2024-02-17 12:38:40 -0500

    Lecture 09: make fizzbuzz even more enterprisy

--- lecture09/fizzbuzz.py
+++ lecture09/fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import os
+import sys
 
 # Functions
 
-def fizzbuzz(start=1, stop=100):
+def fizzbuzz(start: int=1, stop: int=100) -> list[str]:
     ''' 
     >>> fizzbuzz(1, 5)
     1
@@ -19,20 +19,49 @@ def fizzbuzz(start=1, stop=100):
     14
     FizzBuzz
     '''
+    # Version 1: add docstring
+    # Version 2: add type annotations
+    # Version 3: return list (for unit testing)
+    results = []
     for number in range(start, stop + 1):
         if number % 3 == 0 and number % 5 == 0:
-            print('FizzBuzz')
+            results.append('FizzBuzz')
         elif number % 3 == 0:
-            print('Fizz')
+            results.append('Fizz')
         elif number % 5 == 0:
-            print('Buzz')
+            results.append('Buzz')
         else:
-            print(number)
+            results.append(str(number)) # Demonstrate: Fails mypy w/out conversion
+    return results
 
 # Main Execution
 
-def main():
-    fizzbuzz()
+def main(arguments: list[str]=sys.argv[1:]) -> None:
+    # Version 1: no checks (mypy failure)
+    '''
+    start = arguments[0]
+    stop  = arguments[1]
+    '''
+
+    # Version 2: try/except
+    '''
+    try:
+        start = int(arguments[0])
+    except IndexError:
+        start = 1
+    
+    try:
+        stop = int(arguments[1])
+    except IndexError:
+        stop = 100
+    '''
+
+    # Version 3: ternary
+    start = int(arguments[0]) if len(arguments) >= 1 else 1
+    stop  = int(arguments[1]) if len(arguments) >= 2 else 100
+
+    for word in fizzbuzz(start, stop):
+        print(word)
 
 if __name__ == '__main__':
     main()
