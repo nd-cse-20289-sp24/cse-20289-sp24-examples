9a6ad704addb0f3d9ae5df2298b60b071c22c42b
Peter Bui <pbui@nd.edu>
2024-02-27 10:05:57 -0500

    Lecture 12: update translate2

--- lecture12/translate2.py
+++ lecture12/translate2.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
-# curl -sL https://yld.me/raw/c1Dd | cut -d , -f 1 | grep -Eo '^B.*' | sort
+# curl -sL https://yld.me/raw/f3z1 | cut -d , -f 3 | grep -Eo '^B.*' | sort
 
+import csv
 import requests
 
-response   = requests.get('https://yld.me/raw/c1Dd')
+response   = requests.get('https://yld.me/raw/f3z1')
+data       = response.text.splitlines()
 last_names = []
-for line in response.text.splitlines():
-    last_name = line.split(',')[0]
+for netid, first_name, last_name, phone_number in csv.reader(data):
     if last_name.startswith('B'):
         last_names.append(last_name)
+
+for last_name in sorted(last_names):
+    print(last_name)
+
+# Alternative(s)
+
+last_names = [ln for _, _, ln , _ in csv.reader(data) if ln.startswith('B')]
 
 for last_name in sorted(last_names):
     print(last_name)
