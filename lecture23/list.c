0f170153b4c2991ed15018342112d7e2e89e5ee3
Peter Bui <pbui@nd.edu>
2024-04-10 09:40:53 -0400

    Lecture 23: update list.c

--- lecture23/list.c
+++ lecture23/list.c
@@ -11,21 +11,67 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    char path[BUFSIZ];
+/* Node Structure */
 
-    /* Determine directory */
-    if (argc == 2) {
-        strncpy(path, argv[1], BUFSIZ);
-    } else {
-        getcwd(path, BUFSIZ);
+typedef struct Node Node;
+struct Node {
+    char *  path;
+    size_t  size;
+    Node *  next;
+};
+
+Node *  node_create(const char *path, size_t size) {
+    Node *n = calloc(1, sizeof(Node));
+    if (n) {
+        n->path = strdup(path);
+        n->size = size;
     }
+    return n;
+}
 
+void    node_delete(Node *n) {
+    // Base case: invalid Node
+    if (!n) return;
+
+    // Recursive case: delete next Node
+    node_delete(n->next);
+
+    // Release path string
+    free(n->path);
+
+    // Release Node structure
+    free(n);
+}
+
+/* List Structure */
+
+typedef struct {
+    Node *head;
+} List;
+
+void    list_add(List *l, const char *path, size_t size) {
+    Node *n = node_create(path, size);
+
+    if (!l->head) { // Case 1: Empty list
+        l->head = n;
+    } else {        // Case 2: Add to end of list
+        Node *tail = l->head;
+        while (tail->next) {
+            tail = tail->next;
+        }
+
+        tail->next = n;
+    }
+}
+
+/* Functions */
+
+void    add_files(const char *root, List *files) {
     /* Open directory for reading */
-    DIR *d = opendir(path);
+    DIR *d = opendir(root);
     if (!d) {
-        fprintf(stderr, "Unable to opendir on %s: %s\n", path, strerror(errno));
-        return EXIT_FAILURE;
+        fprintf(stderr, "Unable to opendir on %s: %s\n", root, strerror(errno));
+        return;
     }
 
     /* Iterate through directory entries */
@@ -34,28 +80,51 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        printf("%c. %s (%ld)\n", (e->d_type == DT_DIR ? 'D' : 'F'), e->d_name, e->d_ino);
+        // Construct full path
+        char path[BUFSIZ];
+        sprintf(path, "%s/%s", root, e->d_name);
+
+        // Get file metadata
+        struct stat s;
+        if (stat(path, &s) < 0) {
+            continue;
+        }
+
+        // Only add files
+        if (e->d_type == DT_REG) {
+            list_add(files, path, s.st_size);
+        }
     }
 
     closedir(d);
+}
 
-    /* Sorted alternative */
+void    print_files(List *files) {
+    /* Iterate through list of files and print path and size */
+    for (Node *curr = files->head; curr; curr = curr->next) {
+        printf("[%8lu] %s\n", curr->size, curr->path);
+    }
+}
 
-    struct dirent **entries;
-    int n;
+/* Main Execution */
 
-    /* Allocate sorted array of entries */
-    n = scandir(path, &entries, NULL, alphasort);
-    if (n < 0) {
-        fprintf(stderr, "Unable to scandir on %s: %s\n", path, strerror(errno));
-        return EXIT_FAILURE;
+int main(int argc, char *argv[]) {
+    char root[BUFSIZ];
+
+    /* Determine directory */
+    if (argc == 2) {
+        strncpy(root, argv[1], BUFSIZ);
+    } else {
+        getcwd(root, BUFSIZ);
     }
 
-    for (int i = 0; i < n; i++) {
-        puts(entries[i]->d_name);
-        free(entries[i]);   /* Free each entry */
-    }
-    free(entries);          /* Free array of entries */
+    /* Add files and print them */
+    List files = {0};
+    add_files(root, &files);
+    print_files(&files);
+
+    /* Release resources */
+    node_delete(files.head);
 
     return EXIT_SUCCESS;
 }
